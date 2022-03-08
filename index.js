76897d5cad2a02106a7cbd865adb21cfb3d9dffd
Supravat Sarkar <supravatsarkar18@gmail.com>
2022-03-08 10:40:28 +0530

    [update] api host key env var set

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
-var axios = require("axios").default;
+const axios = require("axios").default;
 
 app.use(cors());
 app.use(express.json());
@@ -13,23 +13,26 @@ app.get('/', (req, res) => {
     res.send('Running Youtube To Audio Converter Server.....');
 })
 
-app.post('/getAudio', (res, req) => {
-
-    const videoId = req.body;
+app.post('/getYoutubeToAudio', async (req, res) => {
+    console.log('api key', process.env.RAPID_API_HOST, process.env.API_KEY);
+    const videoId = req.body.videoId;
+    console.log(videoId);
     const options = {
         method: 'GET',
         url: 'https://youtube-mp36.p.rapidapi.com/dl',
-        params: { id: 'UxxajLWwzqY' },
+        params: { id: videoId },
         headers: {
-            'x-rapidapi-host': 'youtube-mp36.p.rapidapi.com',
-            'x-rapidapi-key': '42d5636d4amsha98816696790f3fp1930e0jsn59502490e46b'
+            'x-rapidapi-host': process.env.RAPID_API_HOST,
+            'x-rapidapi-key': process.env.API_KEY
         }
     };
 
     axios.request(options).then(function (response) {
         console.log(response.data);
+        res.json(response.data);
     }).catch(function (error) {
         console.error(error);
+        res.json(error.message);
     });
 })
 
